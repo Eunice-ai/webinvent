0c36d565d2e8b2da2bee382ddbff1aa7f6b974d4
eunice <eunyben4@gmail.com>
2024-08-24 09:12:49 +0100

    feat:add_counter

--- script/main.js
+++ script/main.js
@@ -18,3 +18,32 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+    counter.innerText = '0';
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target');
+        const current = +counter.innerText;
+        const increment = target / 200;
+
+        if (current < target) {
+            counter.innerText = `${Math.ceil(current + increment)}`;
+            setTimeout(updateCounter, 10);
+        } else {
+            counter.innerText = target
+        }
+    };
+
+    const scrollHandler = () => {
+        const rect = counter.getBoundingClientRect();
+        if (rect.top < window.innerHeight && rect.bottom >= 0) {
+            updateCounter();
+            window.removeEventListener('scroll',scrollHandler);
+        }
+    };
+
+    window.addEventListener('scroll', scrollHandler);
+})
